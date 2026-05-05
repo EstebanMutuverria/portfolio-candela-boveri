15202117c080ec295105a21434e62abcde00aeaf
estban03 <estebanmutuverria003@gmail.com>
2026-05-05 01:27:17 -0300

    changes in Certificates

--- src/services/data/certificates.services.data.js
+++ src/services/data/certificates.services.data.js
@@ -6,26 +6,26 @@ import imagen4 from '../../assets/certificates/4to_certificado.jpeg'
 export const CERTIFICATES_LIST = [
     {
         id: 1,
-        name: 'Certificate 1',
-        description: 'Description 1',
+        name: 'Jornada interdicipinaria de actualización profesional odontológica',
+        description: '27/03/2026, San Antonio de Padua, Buenos Aires',
         image: imagen1,
     },
     {
         id: 2,
-        name: 'Certificate 2',
-        description: 'Description 2',
+        name: 'AAON XIX Jornadas estudiantiles',
+        description: '07/06/2025, C.A.B.A',
         image: imagen2,
     },
     {
         id: 3,
-        name: 'Certificate 3',
-        description: 'Description 3',
+        name: 'Jornada interdicipinaria de actualización profesional odontológica',
+        description: '01/11/2025, Merlo, Buenos Aires',
         image: imagen3,
     },
     {
         id: 4,
-        name: 'Certificate 4',
-        description: 'Description 4',
+        name: '42° Congreso internacional de la Asociación Argentina de Odontología',
+        description: 'Del 5 al 7 de noviembre del 2025, C.A.B.A',
         image: imagen4,
     },
 ]
